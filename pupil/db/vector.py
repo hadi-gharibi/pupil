7c9bb49ed2fdd83a769268c8f5eef7113d38aafd
hadi-gharibi <hady.gharibi@gmail.com>
2022-03-04 00:55:28 +0100

    add PandasDB test

--- pupil/db/vector.py
+++ pupil/db/vector.py
@@ -2,8 +2,9 @@ from abc import ABC, abstractmethod
 import faiss
 import numpy as np
 from typing import List
+from pupil.db.config import FaissConf
 
-class BaseVectorDB(ABC):
+class AbstractVectorDB(ABC):
     @abstractmethod
     def train(self,):
         pass
@@ -16,10 +17,19 @@ class BaseVectorDB(ABC):
     def add(self):
         pass
 
+    @property
+    @abstractmethod
+    def n_elements(self):
+        pass
 
 
-class Faiss(BaseVectorDB):
-    def __init__(self, emb_size:np.ndarray, nlist:int, nprobe:int)->None:
+class Faiss(AbstractVectorDB):
+    def __init__(
+        self, 
+        emb_size:np.ndarray, 
+        nlist:int = FaissConf.nlist, 
+        nprobe:int = FaissConf.nprobe
+        )->None:
 
         quantizer = faiss.IndexFlatL2(emb_size)
         self.index = faiss.IndexIVFFlat(quantizer, emb_size, nlist)
@@ -34,3 +44,7 @@ class Faiss(BaseVectorDB):
     def search(self, query , n_results :int = 4) -> List[np.ndarray]:
         distances, inds = self.index.search(query, n_results) 
         return distances, inds # [(self.index.ntotal, n_resutls),  (self.index.ntotal, n_resutls)]
+    
+    @property
+    def n_elements(self):
+        return self.index.ntotal
