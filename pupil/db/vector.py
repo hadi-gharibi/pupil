095a38aeb5fab348de9df9e716646c0624ded80c
hadi-gharibi <hady.gharibi@gmail.com>
2022-03-04 23:20:26 +0100

    abstract to protocol

--- pupil/db/vector.py
+++ pupil/db/vector.py
@@ -1,29 +1,25 @@
 from abc import ABC, abstractmethod
 import faiss
 import numpy as np
-from typing import List
+from typing import Tuple, Protocol
 from pupil.db.config import FaissConf
 
-class AbstractVectorDB(ABC):
-    @abstractmethod
+class VectorDB(Protocol):
     def train(self,):
-        pass
+        ...
 
-    @abstractmethod
     def search(self):
-        pass
+        ...
 
-    @abstractmethod
     def add(self):
-        pass
-
+        ...
+        
     @property
-    @abstractmethod
     def n_elements(self):
-        pass
+        ...
 
 
-class Faiss(AbstractVectorDB):
+class FaissVectorDB:
     def __init__(
         self, 
         emb_size:np.ndarray, 
@@ -41,7 +37,7 @@ class Faiss(AbstractVectorDB):
     def add(self, emb: np.ndarray) -> None:
         self.index.add(emb)
 
-    def search(self, query , n_results :int = 4) -> List[np.ndarray]:
+    def search(self, query , n_results :int = 4) -> Tuple[np.ndarray, np.ndarray]:
         distances, inds = self.index.search(query, n_results) 
         return distances, inds # [(self.index.ntotal, n_resutls),  (self.index.ntotal, n_resutls)]
     
