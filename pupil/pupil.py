fa3b0135d70b488d444285e9fdb3a99b12114595
hadi-gharibi <hady.gharibi@gmail.com>
2022-03-06 01:22:47 +0100

    comment out

--- pupil/pupil.py
+++ pupil/pupil.py
@@ -1,8 +1,8 @@
-from pupil.db.vector import VectorDB
-from pupil.db.index import BaseDB
-from pupil.models.clustering import Clustering
-from typing import Union
+# from pupil.db.vector import VectorDB
+# from pupil.db.meta import BaseDB
+# from pupil.models.clustering import Clustering
+# from typing import Union
 
-class Pupil:
-    def __init__(self, raw_db:BaseDB, vecdb:VectorDB, models:Union[None, Clustering]):
-        pass
+# class Pupil:
+#     def __init__(self, raw_db:BaseDB, vecdb:VectorDB, models:Union[None, Clustering]):
+#         pass
