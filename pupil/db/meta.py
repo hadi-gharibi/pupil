26817dd6b261891c42df2b5fd96716a547f395d8
hadi-gharibi <hady.gharibi@gmail.com>
2022-03-06 01:23:16 +0100

    fix types

---
+++ pupil/db/meta.py
@@ -0,0 +1,62 @@
+import marshmallow_dataclass
+import pandas as pd
+from dataclasses import dataclass
+from typing import Any, Optional, Protocol, Union, List, Any, Dict
+import numpy as np
+from nptyping import NDArray
+import marshmallow_dataclass
+from marshmallow.schema import Schema
+
+class IsDataclass(Protocol):
+    __dataclass_fields__: Dict
+
+class MetaDataDB(Protocol):
+    schema: Schema
+
+    def __init__(self, schema: IsDataclass, label: str):
+        ...
+
+    def add(self, X:pd.DataFrame):
+        ...
+
+    def get(self, index:Union[int, List[int], NDArray[(Any,), np.int32]]) -> List[IsDataclass]:
+        ...
+    def __getitem__(self, i: Union[int, List[int], NDArray[(Any,), np.int32]]):
+        ...
+
+    def __len__(self):
+        ...
+
+class PandasDB:
+    def __init__(self, schema: IsDataclass, label: str) -> None:
+        self.schema = marshmallow_dataclass.class_schema(schema)(many=True) # type: ignore
+        if label not in self.schema.fields.keys(): 
+            raise ValueError(f"{label} must be in your schema. your schema has {[k for k in self.schema.fields.keys()]}")
+        if 'index' in self.schema.fields.keys(): 
+            raise ValueError('Please remove the `index` keyword from your schema')
+        self.label = label
+        self.df:pd.DataFrame = None
+
+    def add(self, data:pd.DataFrame) -> None:
+        if (self.df is None) and (isinstance(data, pd.DataFrame)):
+            if self.label not in (set(data.columns.tolist())):
+                raise ValueError(f"Your DataFrame columns must have `{self.label}`")
+            self.df = pd.DataFrame(data)
+
+        elif (self.df is not None) and (isinstance(data, pd.DataFrame)):
+            self.df = pd.concat([self.df, data], axis=1)
+
+    def get(self, index:Union[int, List[int], NDArray[(Any,), np.int32]]) -> List[IsDataclass]:
+        
+        if isinstance(index, (list, np.ndarray)):
+            data = [self.get(i)[0] for i in index]
+        else:
+            data = self.schema.load( [self.df.iloc[index].to_dict()])
+        
+        return data
+    
+    def __getitem__(self, i: Union[int, List[int], NDArray[(Any,), np.int32]]):
+        return self.get(i)
+
+    def __len__(self):
+        return len(self.df)
