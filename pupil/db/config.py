7a9b27b9660ae293d6d30df707495580e5d5bff4
hadi-gharibi <hady.gharibi@gmail.com>
2022-03-07 01:49:48 +0100

    move NDArray2D

--- pupil/db/config.py
+++ pupil/db/config.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
+from nptyping import NDArray
+from typing import Any, NewType
 
+NDArray2D = NewType("NDArray2D", NDArray[(Any, Any), Any]) # type: ignore
 
 @dataclass
 class FaissConf:
