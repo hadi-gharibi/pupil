aa41cd49bba2a1ca1b2a37994e7f42736ba8d1f4
hadi-gharibi <hady.gharibi@gmail.com>
2022-03-07 01:49:33 +0100

    make uniqe add function for DataBase, add embbeding_search

--- pupil/db/database.py
+++ pupil/db/database.py
@@ -1,6 +1,7 @@
-from pupil.db.vector import VectorDB
+from pupil.db.vector import NDArray2D, VectorDB
 from pupil.db.meta import MetaDataDB
-
+from typing import Any
+from .config import NDArray2D
 
 class DataBase:
     def __init__(self, vecdb: VectorDB, metadb:MetaDataDB):
@@ -8,11 +9,17 @@ class DataBase:
         self.vecdb = vecdb
         self.metadb = metadb
 
-    def add_embeddings(self, embeddings):
+    def add(self, metadata: Any, embeddings: NDArray2D):
+        if len(metadata) != len(embeddings):
+             raise ValueError(f'You have {len(metadata)} data in your metadata and {len(embeddings)} data in your embeddings. These numbers should be same.')
+        self._add_metadata(metadata)
+        self._add_embeddings(embeddings)
+
+    def _add_embeddings(self, embeddings):
         self.vecdb.train(embeddings)
         self.vecdb.add(embeddings)
 
-    def add_metadata(self, data):
+    def _add_metadata(self, data):
         self.metadb.add(data)
 
     def __getitem__(self, i):
@@ -28,4 +35,9 @@ class DataBase:
         if emb :
             res['embeddings'] = self.vecdb[i]
         res['metadata'] = self.metadb[i]
+        return res
+
+    def embbeding_search(self, embeddings: NDArray2D, n_results):
+        _, inds = self.vecdb.search(query = embeddings, n_results=n_results)
+        res = [self.metadb[row] for row in inds]
         return res
