79842349f3bf539457f026acd4f14b208c42ce12
hadi-gharibi <hady.gharibi@gmail.com>
2022-03-07 01:50:09 +0100

    make abc sampler

--- pupil/sampler.py
+++ pupil/sampler.py
@@ -1,22 +1,31 @@
-from typing import Protocol, Union, Optional, Any
+from typing import Union, Optional, Any
 import numpy as np
 from pupil.models.clustering import Clustering
+from abc import ABC, abstractmethod
+from nptyping import NDArray
 
-class Sampler(Protocol):
-    def fit(self, data:np.ndarray, model:Optional[Clustering], labels = Optional[Any]):
-        ...
-
-    def sample(self):
-        ...
-
-class RandomArraySampler:
-    def __init__(self, inds: np.ndarray) -> None:
+class Sampler(ABC):
+    def __init__(self, inds: NDArray[(Any, ), Any]):
+        self._to_pop: NDArray[(Any, ), Any]
         self.inds = inds
-        self.n_elements = len(self.inds)
+        self.strategy()
 
-    def sample(self, n:int = 1) -> np.ndarray:
-        ind = np.random.choice(self.n_elements, n, replace=False)
-        return self.inds[ind]
+    def sample(self, n:int = 1) -> NDArray[(1, ), np.int32]:
+            if self._to_pop.size == 0:
+                raise StopIteration("You already sampled all the data")
+            first, self._to_pop = self._to_pop[:n], self._to_pop[n:] # type: ignore
+            return first
 
-    def fit(self, model:Clustering):
-        pass
+    @abstractmethod
+    def strategy(self):
+        # need to create self._to_pop with the right order. 0 index will sample first
+        ...
+
+class RandomArraySampler(Sampler):
+    def __init__(self, inds: np.ndarray) -> None:
+        self._to_pop: np.ndarray
+        super().__init__(inds = inds)
+
+    def strategy(self):
+        np.random.shuffle(self.inds)
+        self._to_pop = self.inds.copy()
