a70744eef95a81e26bf11cc6b471fd509fc226f6
hadi-gharibi <hady.gharibi@gmail.com>
2022-03-26 05:15:27 +0100

    add docs and defult values

--- pupil/db/vector.py
+++ pupil/db/vector.py
@@ -1,17 +1,18 @@
 import math
-from abc import ABC, abstractmethod
-from typing import Any, List, Optional, Protocol, Tuple, Union
+from typing import Iterable, Optional, Protocol, Tuple, Union
 
 import faiss
 import numpy as np
-from nptyping import NDArray
-from pupil.db.config import FaissConf
-from scipy import spatial
-
-from .config import NDArray2D, SimilarityType
+from pupil.types import Distance, NDArray2D
 
 
 class VectorDB(Protocol):
+    """Vector database to save the embeddings and fast distance calculation.
+    Normal usage: 
+        1. Create object
+        2. `build_index`
+        3. `add` embeddings
+    """
     def search(
         self, query: NDArray2D, n_results: int = 4
     ) -> Tuple[NDArray2D, NDArray2D]:
@@ -52,9 +53,9 @@ class VectorDB(Protocol):
 class FaissVectorDB:
     def __init__(
         self,
-        similarity_metric: SimilarityType = FaissConf.similarity_type,
-        nlist: Optional[int] = FaissConf.nlist,
-        nprobe: Optional[int] = FaissConf.nprobe,
+        similarity_metric: Distance = Distance.COSINE,
+        nlist: Optional[int] = None ,
+        nprobe: Optional[int] = 5,
     ) -> None:
 
         self.similarity_metric = similarity_metric
@@ -81,43 +82,45 @@ class FaissVectorDB:
 
         if not self.index.is_trained:
             self.index.train(embeddings)
-        if self.similarity_metric == SimilarityType.COSINE:
+        if self.similarity_metric == Distance.COSINE:
             faiss.normalize_L2(embeddings)
         self.add(embeddings)
 
-    def __getitem__(self, i: Union[int, List[int], NDArray[(Any,), np.int32]]):
+    def __getitem__(self, i: Union[int, Iterable[int], slice]):
         if isinstance(i, slice):
-            start = i.start
-            if not start:
-                start = 0
-
-            stop = i.stop
-            if not stop:
-                stop = self.__len__()
-
-            step = i.step
-            if not step:
-                step = 1
-
-            i = list(range(start, stop, step))
+            i = list(range(*i.indices(self.__len__()))) # type: ignore
 
         if not self.index.is_trained:
             raise ValueError("First add data to the database.")
 
-        if type(i) is not int:
-            return np.vstack([self.__getitem__(ind) for ind in i])  # type: ignore
+        if not isinstance(i, int):
+            return np.vstack([self.__getitem__(ind) for ind in i])
 
         if hasattr(self.index, "make_direct_map"):
             self.index.make_direct_map()
         return self.index.reconstruct(i).reshape(1, -1).astype(np.float32)
 
     def add(self, embeddings: NDArray2D) -> None:
+        """Add embeddings into the database
+
+        Args:
+            embeddings (NDArray2D):
+        """
         self.index.add(embeddings)
 
     def search(
         self, query: NDArray2D, n_results: int = 4
     ) -> Tuple[NDArray2D, NDArray2D]:
-        if self.similarity_metric == SimilarityType.COSINE:
+        """Search embeddings to get the closest embeddings to the queries.
+
+        Args:
+            query (NDArray2D): Vectors to search
+            n_results (int, optional): Number of results per query. Defaults to 4.
+
+        Returns:
+            Tuple[NDArray2D, NDArray2D]: Return (Distances, indices)
+        """
+        if self.similarity_metric == Distance.COSINE:
             faiss.normalize_L2(query)
         distances, inds = self.index.search(query, n_results + 1)
         return (
