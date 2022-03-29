68e61fe736f1420a39628e9bfad3456979e8427d
hadi-gharibi <hady.gharibi@gmail.com>
2022-03-30 00:13:56 +0200

    add Distance

--- pupil/types.py
+++ pupil/types.py
@@ -1,9 +1,18 @@
-from typing import Any, Dict, NewType, Protocol
+from enum import Enum, auto
+from typing import Any, Dict, Iterable, NewType, Protocol, Sequence, TypeVar
 
 from nptyping import NDArray
 
 NDArray2D = NewType("NDArray2D", NDArray[(Any, Any), Any])  # type: ignore
+T = TypeVar("T")
 
 
 class IsDataclass(Protocol):
     __dataclass_fields__: Dict
+
+class Distance(Enum):
+    COSINE = auto()
+    DOT_PRODUCT = auto()
+
+
+SeqOfIndices = TypeVar("SeqOfIndices", bound= Sequence[int])
