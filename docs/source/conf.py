d39bca90e80de6a16062448e481cc8c3a12879f4
hadi-gharibi <hady.gharibi@gmail.com>
2022-04-02 01:12:07 +0200

    bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Hadi Gharibi"
 author = "Hadi Gharibi"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0-alpha.0"
+release = "0.2.0"
 
 
 # -- General configuration ---------------------------------------------------
