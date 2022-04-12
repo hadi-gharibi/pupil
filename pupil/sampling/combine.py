9d266572cc1ebf659e87206be6e5f1548959d510
hadi-gharibi <hady.gharibi@gmail.com>
2022-04-12 02:02:40 +0200

    add ATLASSampler

--- pupil/sampling/combine.py
+++ pupil/sampling/combine.py
@@ -1,11 +1,16 @@
 import math
+from cProfile import label
+from typing import Any
 
 import numpy as np
+from nptyping import NDArray
 from pupil.models import FaissKMeansClustering
 from pupil.sampling.base import RandomSampler
 from pupil.sampling.cluster_based import ClusteringSampler
 from pupil.sampling.uncertainty import UncertaintySampler, entropy_based
 from pupil.types import NDArray2D
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
 
 
 class ClusteredUncertaintySampler:
@@ -69,6 +74,103 @@ class HighEntropyClusterSampler:
         self.indices_ = rs.indices_
 
 
+class ATLASSampler:
+    """
+    Active transfer learning for adaptive sampling.
+    validation items are predicted by the model and bucketed as “Correct” or
+    “Incorrect,” according to whether they were classified correctly.
+    The last layer of the model is retrained to predict whether items
+    are “Correct” or “Incorrect,” effectively turning the two buckets into new labels.
+    We apply the new model to the unlabeled data, predicting whether each item
+    will be “Correct” or “Incorrect.” We can sample the most likely to be
+    “Incorrect.”
+    """
+
+    def __init__(
+        self,
+        trust_model=None,
+        uncertainty_strategy="entropy_based",
+    ):
+        """_summary_
+
+        Args:
+            trust_model (optional): Any Sklean Classifier if None, then will use LogisticRegression.
+                Defaults to None.
+            uncertainty_strategy (str):
+                One of
+                ['least_confidence', 'margin_confidence', 'ratio_confidence', 'entropy_based'].
+                 Defaults to "entropy_based".
+        """
+        self.scaler = StandardScaler()
+        self.trust_model = trust_model
+        if self.trust_model is None:
+            self.trust_model = LogisticRegression(
+                solver="saga",
+                n_jobs=-1,
+                penalty="l1",
+                max_iter=1000,
+            )
+        self.unc_sampler = UncertaintySampler.from_strategy(uncertainty_strategy)
+
+    def fit(
+        self,
+        unlabeled_emb: NDArray2D,
+        validation_y_hat_prob: NDArray2D,
+        validation_emb: NDArray2D,
+        validation_y: np.ndarray,
+    ):
+        """
+        Steps:
+            1. Scale embs data
+            2. Use `validation_y_hat_prob` to see which class model picked
+            3. Compare results of 2 and validation_y and tag them with corr/incorr
+            4. Train `trust_model` on this new generated tags from step 3
+            5. Use model from step 4 to predict tags for all `unlabeled_emb`
+            6. Sample from predictions of 5 and pick select indecies with highest
+            chance of `incorrect` tags
+        Args:
+            unlabeled_emb (NDArray2D): Embs of trained data from trained model
+            validation_y_hat_prob (NDArray2D): Predictions of a model on validation set
+            validation_emb (NDArray2D): Embs of validation data from trained model
+            validation_y (np.ndarray): Actual lables of validation set
+        """
+
+        unlabeled_emb = self.scaler.fit_transform(
+            unlabeled_emb
+        )  # scale input for saga solver
+        validation_emb = self.scaler.transform(validation_emb)
+        val_y_hat = np.argmax(
+            validation_y_hat_prob, axis=1
+        )  # change prob to actual preds
+        val_y_corr_incorr = np.where(
+            validation_y == val_y_hat, "correct", "incorrect"
+        )  # label 0 to correct and 1 to incorrect
+
+        self.trust_model.fit(
+            validation_emb, val_y_corr_incorr
+        )  # train model to predict if it will be corr/incorr
+        unlabeled_y_hat_probs = self.trust_model.predict_proba(
+            unlabeled_emb
+        )  # predict the corr/incorr
+
+        incorrect_filter = (
+            np.argmax(unlabeled_y_hat_probs, axis=1) == 1
+        )  # select the ones that will be incorrect
+
+        # since we filter, we need a mapper to understand what inds in the filtered array represnt in original array
+        inds = np.argwhere(incorrect_filter).flatten().tolist()
+        rev_inds_mapper = {i: ind for i, ind in enumerate(inds)}
+
+        incorrect_y_hat_prob = unlabeled_y_hat_probs[
+            incorrect_filter
+        ]  # probability this would be incorrect
+        self.unc_sampler.fit(incorrect_y_hat_prob)
+
+        # select the one models most certain it would be classified incorrectly
+        inds_ = self.unc_sampler.indices_.tolist()[::-1]  # type: ignore
+        self.indices_ = [*map(rev_inds_mapper.get, inds_)]
+
+
 if __name__ == "__main__":
     all_data_size = 300
     dim = 60
@@ -76,8 +178,10 @@ if __name__ == "__main__":
     data_in_a_cluster = all_data_size / n_clusters
     train_size = int(data_in_a_cluster * 3.5)
 
+    import pandas as pd
     from sklearn.datasets import make_biclusters
     from sklearn.linear_model import LogisticRegression
+    from sklearn.model_selection import train_test_split
 
     data, rows, columns = make_biclusters(
         shape=(all_data_size, dim),
@@ -90,13 +194,26 @@ if __name__ == "__main__":
     labels = [i // data_in_a_cluster for i in range(all_data_size)]
     rng = np.random.RandomState(0)
 
-    train_data = data[:train_size]
-    unlabeled_data = data[train_size:]
-    clf = LogisticRegression(random_state=0, solver="liblinear").fit(
-        train_data, labels[:train_size]
+    train_val_x = data[:train_size]
+    train_val_y = labels[:train_size]
+    tr_x, val_x, tr_y, val_y = train_test_split(
+        train_val_x, train_val_y, train_size=0.8
     )
-    probs = clf.predict_proba(unlabeled_data)
-    print(probs)
-    sampler = HighEntropyClusterSampler(n_clusters=2)
-    sampler.fit(prob_predictions=probs, unlabeled_data=unlabeled_data)
-    print(sampler.indices_)
+
+    unlabeled_data = data[train_size:]
+    clf = LogisticRegression(random_state=0, solver="liblinear").fit(tr_x, tr_y)
+
+    probs = clf.predict_proba(val_x)
+    sampler = ATLASSampler()
+    sampler.fit(
+        unlabeled_emb=unlabeled_data,
+        validation_emb=val_x,
+        validation_y=np.array(val_y),
+        validation_y_hat_prob=probs,
+    )
+    preds = clf.predict_proba(unlabeled_data)
+    preds = np.argmax(preds, axis=1).reshape((-1, 1))
+    preds = preds[sampler.indices_]
+    actual = labels[train_size:]
+    df = pd.DataFrame([preds, actual], columns=["preds", "actual"])
+    print(df)
